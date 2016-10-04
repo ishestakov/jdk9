857075ba2384935bd91a780296710c0306791034
ilyuhazp <ilyuhazp@gmail.com>
2016-10-04 14:57:22 +0200

    Remove unnecessary modules

--- spring-exampl-module-extract-test/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/lib/jvm/jdk-9/bin/java -mp 'mod:bin/mods:main/resources' -addmods spring.core,spring.aop,spring.expression -cp './lib/*:main/resources' -m com.github.ishestakov.coe.pass.app/com.github.ishestakov.coe.Main
