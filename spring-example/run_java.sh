d0ae1be1395a131daf6cd0ac5ec4107138e7821b
Illia Shestakov <ilyuhazp@gmail.com>
2017-06-02 14:00:34 +0200

    Fix build and run scripts

--- spring-example/run_java.sh
+++ spring-example/run_java.sh
@@ -5,7 +5,7 @@ popd > /dev/null
 
 JAVA_VERSION=$1;
 
-docker run -it --rm -v $SCRIPTPATH:/example -v $SCRIPTPATH/target/java$JAVA_VERSION:/example/target openjdk:$JAVA_VERSION java -cp '/example/lib/*:/example/target/:/example/target/resources' $JAVA_SPEC_ARGS com.github.ishestakov.coe.Main
+docker run -it --rm -v $SCRIPTPATH/lib/:/example/lib -v $SCRIPTPATH/target/java$JAVA_VERSION:/example/target openjdk:$JAVA_VERSION java -cp '/example/lib/*:/example/target/classes:/example/target/resources' com.github.ishestakov.coe.Main
 	
 
 
